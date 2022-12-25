5387ef72720a5411d1e649a2a020d8a207b28c6f
Aleh Belanouski <aleh_belanouski@epam.com>
2022-12-25 22:35:15 +0300

    Changes for create product endpoint for task 6

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,10 @@
 const API_PATHS = {
-  product: "https://5h99mxgyil.execute-api.eu-west-1.amazonaws.com/products",
+  product: "https://16zpz4tfm7.execute-api.eu-west-1.amazonaws.com/products",
+  // product: "https://5h99mxgyil.execute-api.eu-west-1.amazonaws.com/products",
   // product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://bormk75cml.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://5h99mxgyil.execute-api.eu-west-1.amazonaws.com/products",
+  bff: "https://16zpz4tfm7.execute-api.eu-west-1.amazonaws.com/products",
   // bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
