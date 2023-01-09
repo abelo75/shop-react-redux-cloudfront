b890005444f999a16534db66acde6a3982c0ba08
Aleh Belanouski <aleh_belanouski@epam.com>
2023-01-09 22:28:55 +0300

    Changes for task #8. Remove network mock and add orders and carts endpoints

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,11 +2,11 @@ const API_PATHS = {
   product: "https://16zpz4tfm7.execute-api.eu-west-1.amazonaws.com/products",
   // product: "https://5h99mxgyil.execute-api.eu-west-1.amazonaws.com/products",
   // product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  order: "https://onsx99gysa.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://bormk75cml.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://16zpz4tfm7.execute-api.eu-west-1.amazonaws.com/products",
   // bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  cart: "https://onsx99gysa.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
 export default API_PATHS;
